b5c6ffee7453b10a8bc9da85c3e6487607ef5296
Elaidzha1940 <eli080293@gmail.com>
2023-11-04 19:06:07 +0300

    Update ContentView

--- CoreData/ContentView.swift
+++ CoreData/ContentView.swift
@@ -13,12 +13,14 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
-//    @FetchRequest(
-//        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-//        animation: .default)
-//    private var items: FetchedResults<Item>
+    //    @FetchRequest(
+    //        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+    //        animation: .default)
+    //    private var items: FetchedResults<Item>
     
-    @FetchRequest(entity: VegieEntity.entity(), sortDescriptors: []) var vegies: FetchedResults<VegieEntity>
+    @FetchRequest(entity: VegieEntity.entity(),
+                  sortDescriptors: [])
+    var vegies: FetchedResults<VegieEntity>
     
     var body: some View {
         NavigationView {
@@ -31,10 +33,10 @@ struct ContentView: View {
                         //Text(item.timestamp!, formatter: itemFormatter)
                     }
                 }
-                //.listStyle(PlainListStyle)
-                .navigationTitle("Vegies")
-                //.onDelete(perform: deleteItems)
+                .onDelete(perform: deleteItems)
             }
+            .navigationTitle("Vegies")
+            
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
@@ -47,38 +49,35 @@ struct ContentView: View {
             }
             Text("Select an item")
         }
+        .listStyle(PlainListStyle())
     }
     
     private func addItem() {
         withAnimation {
             let newVegie = VegieEntity(context: viewContext)
             newVegie.name = "Potato"
-//            let newItem = Item(context: viewContext)
-//            newItem.timestamp = Date()
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            saveItems()
         }
     }
     
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            guard let index = offsets.first else { return }
+            let vegieEntity = vegies[index]
+            viewContext.delete(vegieEntity)
+            // offsets.map { items[$0] }.forEach(viewContext.delete)
+            saveItems()
+        }
+    }
+    
+    private func saveItems() {
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
